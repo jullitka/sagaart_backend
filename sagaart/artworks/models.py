c569cc038460e907c538f04faa3ed40bbcc066c9
jullitka <julia_vadimovna_@mail.ru>
2024-06-17 00:01:35 +0300

    add models education, artist, series, exhebitions

--- sagaart/artworks/models.py
+++ sagaart/artworks/models.py
@@ -1,5 +1,145 @@
 from django.db import models
 
+from constants import ORIENTATION_CHOICES, SALE_STATUS_CHOICES
+
+
+class EducationModel(models.Model):
+    """Модель образовательного учрежления"""
+    name = models.CharField(
+        verbose_name='Название образовательного учреждения',
+        null=False,
+        blank=False,
+        max_length=200
+    )
+
+    class Meta:
+        verbose_name = 'Образовательное учреждение'
+        verbose_name_plural = 'Образовательные учреждения'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class ArtistModel(models.Model):
+    """Модель автора произведений"""
+    name = models.CharField(
+        verbose_name='Имя и фамилия',
+        null=False,
+        blank=False,
+        max_length=200
+    )
+    education = models.ForeignKey(
+        EducationModel,
+        verbose_name='Образование автора',
+    )
+    about_artist = models.CharField(
+        verbose_name='Об авторе',
+        null=False,
+        blank=False,
+        max_length=200
+    )
+    photo = models.ImageField(
+        verbose_name='Фото автора'
+    )
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class EducationAuthorModel(models.Model):
+    """Модель связи образотвальельных учреждени и атворов"""
+    artist = models.ForeignKey(
+        ArtistModel,
+        verbose_name='Имя и фамилия автора'
+    )
+    education = models.ForeignKey(
+        EducationModel,
+        verbose_name='Название учебного заведения'
+    )
+    education_start = models.IntegerField(
+        verbose_name='Год начала обучения',
+        blank=True,
+        null=True
+    )
+    education_finish = models.IntegerField(
+        verbose_name='Год окончания обучения',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return f'{self.artist} учился в {self.education}'
+
+
+class SeriesModel(models.Model):
+    """Модель серии работ"""
+    name = models.CharField(
+        verbose_name='Название серии',
+        null=False,
+        blank=False,
+        max_length=200
+    )
+    author = models.ForeignKey(
+        ArtistModel,
+        verbose_name='Автор серии'
+    )
+
+    class Meta:
+        verbose_name = 'Серия работ'
+        verbose_name_plural = 'Серии работ'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class ExhibitionModel(models.Model):
+    """Модель выставки"""
+    name = models.CharField(
+        verbose_name='Название выставки',
+        null=False,
+        blank=False,
+        max_length=200
+    )
+    period = models.CharField(
+        verbose_name='Период проведения выставки',
+        null=False,
+        blank=False,
+        max_length=50
+    )
+    address = models.CharField(
+        verbose_name='Место проведения выставки',
+        null=False,
+        blank=False,
+        max_length=200
+    ) 
+
+    class Meta:
+        verbose_name = 'Выставка'
+        verbose_name_plural = 'Выставки'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class ExhibitionParticipantModel(models.Model):
+    """Модель связи выставок с их участниками"""
+    exhibition = models.ForeignKey(
+        ExhibitionModel,
+        verbose_name='Выставка'
+    )
+    participant = models.ForeignKey(
+        ArtistModel,
+        verbose_name = 'Участник выставки'
+    )
+
+    def __str__(self):
+        return f'{self.participant} участвовал в {self.exhibition}'
+
+
 class ArtworkModel(models.Model):
     """Модель произведения искусства."""
     name = models.CharField(
@@ -11,7 +151,7 @@ class ArtworkModel(models.Model):
     author = models.ForeignKey(
         ArtistModel,
         verbose_name='Автор работы',
-        n_delete=models.CASCADE
+        on_delete=models.CASCADE
     )
     user = models.ForeignKey(
         UserModel,
@@ -35,30 +175,36 @@ class ArtworkModel(models.Model):
         blank=False,
         max_length=200
     )
-    is_on_estimating = models.BooleanField(
-        verbose_name='Находится на оценивании',
+    series = models.ForeignKey(
+        SeriesModel,
+        verbose_name='Серия работ'
+    )
+    is_estimate = models.BooleanField(
+        verbose_name='Оценка получена',
         default=False
     )
-    is_on_sold = models.BooleanField(
-        verbose_name='Находится на продаже',
-        default=False
+    is_on_sold = models.CharField(
+        verbose_name='Статус продажи',
+        choices_name=SALE_STATUS_CHOICES,
+        null=False,
+        blank=False,
+        default='не размещено'
     )
+    # может быть пустым в случае, если произведение - не картина
     orientation = models.CharField(
         verbose_name='Ориентация работы',
-        null=False,
-        blank=False,
+        choices=ORIENTATION_CHOICES,
+        null=True,
+        blank=True,
         max_length=100
     )
-    style = models.ForeignKey(
-        StyleModel,
-        verbose_name='Стиль работы'
+    style = models.CharField(
+        verbose_name='Стиль работы',
+        null=True,
+        blank=True,
+        max_length=100
     )
-    series = models.CharField(
-        verbose_name='Серия',
-        null=False,
-        blank=False,
-        max_length=200
-    )
+
     decoration = models.CharField(
         verbose_name='Оформление',
         null=False,
@@ -66,7 +212,7 @@ class ArtworkModel(models.Model):
         max_length=200
     )
     author_signature = models.CharField(
-        verbose_name='Оформление',
+        verbose_name='Наличие авторской подписи',
         null=False,
         blank=False,
         max_length=200
@@ -96,10 +242,11 @@ class ArtworkPrice(models.Model):
         blank=False,
         max_length=200
     )
+    # может быть пустым, если произведение - не картина
     copy_price = models.CharField(
-        verbose_name='Цена оригинала',
-        null=False,
-        blank=False,
+        verbose_name='Цена постера',
+        null=True,
+        blank=True,
         max_length=200
     )
     pub_date = models.DateTimeField(
