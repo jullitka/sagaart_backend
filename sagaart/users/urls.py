a1270489345cc779807b3b9c913ac999d0d674b3
Maksim Kozlov <mkvrs68@gmail.com>
2024-06-17 04:13:12 +0300

    v.0.1

--- sagaart/users/urls.py
+++ sagaart/users/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 from . import views
 
-v1_router = routers.SimpleRouter()
+v1_router = routers.DefaultRouter()
 
 v1_router.register(
     'users', views.UsersViewSet, basename='users'
 )
+
+urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+]
