98f17a2360b26801f8a9c4509f3515c5c6a30e08
jullitka <julia_vadimovna_@mail.ru>
2024-06-21 14:04:08 +0300

    fix

--- sagaart/sagaart/settings.py
+++ sagaart/sagaart/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
-DEBUG = strtobool(os.getenv("DEBUG", "False"))
+DEBUG = strtobool(os.getenv("DEBUG", "True"))
 
 #ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(',')
 
