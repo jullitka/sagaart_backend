0af4242930842deff233978f34ea601b4b864827
jullitka <julia_vadimovna_@mail.ru>
2024-06-21 16:03:26 +0300

    fix

--- sagaart/sagaart/settings.py
+++ sagaart/sagaart/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static-back/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-back')
 
-MEDIA_URL = '/media-back/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media-back')
 
 # Default primary key field type
